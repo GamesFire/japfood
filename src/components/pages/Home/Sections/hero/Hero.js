0f13572bacb8475315c5beaa541b84216c5cd4e8
Lobix <lobix2005@gmail.com>
2023-10-11 22:38:20 +0200

    Update header and create hero section on home page

---
+++ src/components/pages/Home/Sections/hero/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./hero.module.css";
+import Container from "../../../../container/Container";
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <Container>
+        <h1 className={styles.title}>
+          Відкрийте для себе мистецтво японських страв
+        </h1>
+      </Container>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "./Sections/Hero";
+import Hero from "./Sections/hero/Hero";
 import PopularFood from "./Sections/PopularFood";
 
 const HomePage = () => {
